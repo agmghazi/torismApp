d4e3b8a5632cd7694c826a1153e4989665ad42a8
ahmed Gamal <agmghazi@hotmail.com>
2022-03-29 12:14:44 +0300

    Auto-generated commit [ci skip]

--- polyfills.js
+++ polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/ahmedmac/Desktop/torismApp/new/tourismApp/src/polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! E:\sotwares\tourismApp\torismApp\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
